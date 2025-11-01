cd6516d7b5bc0c4352d004ee59da0dc609eda480
Pody <119523069+PodySIN@users.noreply.github.com>
2025-11-01 13:57:28 +0300

    Add input and output

--- main.cpp
+++ main.cpp
@@ -30,20 +30,64 @@ int** create_matrix(size_t rows, size_t cols)
 
 void costruct(int **mtx, int init, size_t rows, size_t cols)
 {
-    for (int i = 0; i < rows; ++i)
+    for (size_t i = 0; i < rows; ++i)
 	{
-	    for (int j = 0; j < cols; ++j)
+	    for (size_t j = 0; j < cols; ++j)
 	    {
 	        mtx[i][j] = init;
 	    }
 	}
 }
 
+void input(int **mtx, size_t rows, size_t cols)
+{
+    for (size_t i = 0; i < rows; ++i)
+	{
+	    for (size_t j = 0; j < cols; ++j)
+	    {
+	        std::cin >> mtx[i][j];
+	    }
+	    if (std::cin.bad())
+	    {
+	        throw std::logic_error("Internal stream error!");
+	    }
+	    else if (std::cin.eof())
+	    {
+	        if (i != rows - 1)
+	        {
+	            throw std::logic_error("Unexpected eof!");
+	        }
+	    }
+	    else if (std::cin.fail())
+	    {
+	        throw std::logic_error("Unexpected input!");
+	   }
+	}
+}
+
+void output(int **mtx, size_t rows, size_t cols)
+{
+    for (size_t i = 0; i < rows; ++i)
+    {
+        for (size_t j = 0; j < cols; ++j)
+        {
+            if (j == 0)
+            {
+                std::cout << mtx[i][j];
+            }
+            else
+            {
+                std::cout << " " << mtx[i][j];
+            }
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() 
 {
     size_t rows = 5, cols = 5;
     std::cin >> rows >> cols;
-    
     if (std::cin.bad())
 	{
 	    std::cerr << "Internal stream error!\n";
@@ -71,30 +115,15 @@ int main()
 	    return 1;
 	}
 	costruct(mtx, 1, rows, cols);
-	for (int i = 0; i < rows; ++i)
+	try
 	{
-	    for (int j = 0; j < cols; ++j)
-	    {
-	        std::cin >> mtx[i][j];
-	    }
-	    if (std::cin.bad())
-	    {
-	        std::cerr << "Internal stream error!\n";
-	        return 2;
-	    }
-	    else if (std::cin.eof())
-	    {
-	        if (i != rows - 1)
-	        {
-	            std::cerr << "Unexpected eof!\n";
-	            return 2;
-	        }
-	    }
-	    else if (std::cin.fail())
-	    {
-	        std::cerr << "Unexpected input!\n";
-	        return 2;
-	   }
+	    input(mtx, rows, cols);
 	}
+	catch (const std::logic_error &e)
+	{
+	    std::cerr << e.what() << "\n";
+	    return 2;
+	}
+	output(mtx, rows, cols);
 	destroy_matrix(mtx, rows);
 }
