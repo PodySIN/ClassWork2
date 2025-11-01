79717785d93702f2b33deff10ecb100897e7627d
Pody <119523069+PodySIN@users.noreply.github.com>
2025-11-01 13:26:47 +0300

    Add construct function

--- main.cpp
+++ main.cpp
@@ -28,18 +28,30 @@ int** create_matrix(size_t rows, size_t cols)
     return mtx;
 }
 
+void costruct(int **mtx, int init, size_t rows, size_t cols)
+{
+    for (int i = 0; i < rows; ++i)
+	{
+	    for (int j = 0; j < cols; ++j)
+	    {
+	        mtx[i][j] = init;
+	    }
+	}
+}
+
 int main() 
 {
-    
+    size_t rows = 5, cols = 5;
 	int **mtx = nullptr;
 	try
 	{
-	    mtx = create_matrix(5, 5);
+	    mtx = create_matrix(rows, cols);
 	}
 	catch (const std::bad_alloc & e)
 	{
 	    std::cerr << e.what() << "\n";
 	    return 1;
 	}
-	destroy_matrix(mtx, 5);
+	costruct(mtx, 1, rows, cols);
+	destroy_matrix(mtx, rows);
 }
